44aaa81c2687d9d4fe7e86868cd4415dc262c259
Steve Dudek <stevedudek@gmail.com>
2014-11-03 09:10:39 -0800

    Socket.io implementation
    Attempting github application to send messages to local server

---
+++ app.js
@@ -0,0 +1,48 @@
+// Node server that takes messages
+// from the Rock Paper Scissors game
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(8888);
+
+app.use('http://stevedudek.github.io/RockPaperScissors/public'));
+// app.use(express.static(__dirname + '/public'));
+/*
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+*/
+
+io.on('connection', function (socket) {
+  socket.on('player', function (player) {
+    console.log("Player chooses " + player);
+  });
+  socket.on('computer', function (computer) {
+    console.log("Computer chooses " + computer);
+  });
+  socket.on('result', function (result) {
+    console.log("Player " + result + "s");
+  });
+  socket.on('pscore', function (score) {
+    console.log("Player has " + score + " points");
+  });
+  socket.on('cscore', function (score) {
+    console.log("Computer has " + score + " points");
+  });
+	/*
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+*/
+});
+
+/*
+// __dirname + '/public'  = ./public
+// Will look for index.html in public directory
+app.use(express.static(__dirname + '/public'));
+
+app.listen(8888);
+*/
---
+++ public/sketch.js
@@ -0,0 +1,176 @@
+// Socket messenger
+var socket = io('http://localhost');
+
+var stage = "start";
+var win_lose_draw = "";
+var player_choice = "question";
+var computer = "question";
+var player_score = 0;
+var computer_score = 0;
+var compTimer = false;
+
+// text('sketch.js is loading');
+
+function setup() {
+  // put setup code here
+  var result = createCanvas(900,140);
+  result.parent('result');
+}
+
+function draw() {
+  // put drawing code here
+  drawScreen();
+}
+
+function computer_pick() {
+	
+	var number = Math.floor(Math.random()*3);	// 0,1, or 2
+	switch (number) {
+		case 0:
+			computer = "rock";
+			break;
+		case 1:
+			computer = "paper";
+			break;
+		case 2:
+			computer = "scissors";
+			break;
+		default:
+			break;
+	}
+	var compImg = document.getElementById("compPic");
+	compImg.src = computer + ".jpg"; 	// string concatenation
+}
+
+function assess(player, computer) {
+	// Tell the server the final choices
+	socket.emit('player', player);
+	socket.emit('computer', computer);
+
+	// Update the images for player and computer
+	var playerFinalImg = document.getElementById("playerChoice");
+	playerFinalImg.src = player + ".jpg";
+
+	var compFinalImg = document.getElementById("compChoice");
+	compFinalImg.src = computer + ".jpg";
+
+	if (player == "nuke") {
+		win_lose_draw = "win";
+		return;
+	}
+
+	if (player == computer) {
+		win_lose_draw = "draw";
+		return;
+	}
+
+	switch (player) {
+		case "rock":
+			if (computer == "paper") {
+				win_lose_draw = "lose";
+				return;
+			} else {
+				win_lose_draw = "win";
+				return;
+			}
+			break;
+		case "paper":
+			if (computer == "scissors") {
+				win_lose_draw = "lose";
+				return;
+			} else {
+				win_lose_draw = "win";
+				return;
+			}
+			break;
+		case "scissors":
+			if (computer == "paper") {
+				win_lose_draw = "win";
+				return;
+			} else {
+				win_lose_draw = "lose";
+				return;
+			}
+			break;
+		default:
+			win_lose_draw = "draw";
+			break;
+	}
+}
+
+$(document).ready(function(){
+  $("#rock_but").click(function(){
+  	player_choice = "rock";
+	stage = "game";
+  });
+  $("#paper_but").click(function(){
+  	player_choice = "paper";
+	stage = "game";
+  });
+  $("#scissors_but").click(function(){
+  	player_choice = "scissors";
+	stage = "game";
+  });
+  $("#compPic").click(function(){
+  	player_choice = "nuke";
+	stage = "game";
+  });
+});
+
+function updateScore(result) {
+	if (result == "win") {
+		player_score++;
+	}
+	if (result == "lose") {
+		computer_score++;
+	}
+}
+
+function drawScreen() {	
+	fill(0);
+ 	textFont('Helvetica');
+
+ 	text('Beginning the game');
+	switch (stage)  {
+		case "start":
+			
+			if (!compTimer) {
+				var Timer = 2000;
+				if (player_score > computer_score) {
+					Timer = Timer - (150 *(player_score - computer_score));
+					if (Timer < 50) {
+						Timer = 0;
+					}
+				}
+				compTimer = setInterval(function(){computer_pick()}, Timer);
+			}
+			break;
+		case "game":
+			clearInterval(compTimer);
+			// computer_pick();
+			assess(player_choice, computer);
+			stage = "finale";
+		case "finale":
+			background(255);
+			textSize(64);
+			textAlign(CENTER);
+			text(win_lose_draw, 420, 60);
+			updateScore(win_lose_draw);
+			textSize(32);
+			textAlign(CENTER);
+			text("You:" + player_score + "  Computer:" + computer_score, 340, 120);
+			
+			// Broadcast result to server
+			socket.emit('result', win_lose_draw);
+			socket.emit('pscore', player_score);
+			socket.emit('cscore', computer_score);
+
+			computer_pick();
+			compTimer = false;
+			stage = "start";
+			break;
+		default:
+			text("Something wrong happened");
+			break;
+	}			
+}
